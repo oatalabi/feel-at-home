1550cf1f4a4de7ca4e7853c04309998c6b1c3b05
Seyi Talabi <oatalabi@gmail.com>
2019-04-23 13:57:38 -0700

    all but school features

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+var cors = require('cors');
 
 app.get('/', (req, res) => res.sendFile('index.html', {
     root: path.join(__dirname)
 }))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-app.listen(process.env.PORT || 3000)
+app.use(cors());
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(process.env.PORT || 3000)
